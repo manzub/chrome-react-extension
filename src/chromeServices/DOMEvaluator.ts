c6da5fac507c858b0e2759d5c0f0e24d7f51d5b1
WizzDom <41767916+manzub@users.noreply.github.com>
2023-04-03 19:18:53 +0100

    updated dom listener for modern sites

--- src/chromeServices/DOMEvaluator.ts
+++ src/chromeServices/DOMEvaluator.ts
@@ -1,5 +1,7 @@
 import { DOMMessage, DOMMessageResponse, VaultItem } from "../types";
 
+type CurrItem = { origin: string, pathname: string, value: string, email: string, username: string };
+
 // TODO: fix listener
 const messagesFromReactAppListener = (message: DOMMessage, sender: chrome.runtime.MessageSender, sendResponse: (response: DOMMessageResponse) => void) => {
   console.log('[content.js]. Message received', message.type);
@@ -11,88 +13,100 @@ const messagesFromReactAppListener = (message: DOMMessage, sender: chrome.runtim
     let vaultItems: VaultItem[] = result.vaultItems
     // DONE: listen for form submits
     if (message.type === 'LISTEN_DOM') {
+      // new event listener
+      var listenerInterval = setInterval(function () { // interval to check page updates and listener
+        try {
+          let currItem: CurrItem = JSON.parse((sessionStorage.getItem('currItem') || '{}'));
+          if (currItem.pathname && currItem.value) { // if an item for this instance has been saved
+            if (currItem.pathname !== window.location.pathname) { // a redirect happened
+              let itemExists = vaultItems.find(x => x.web_url === currItem.origin);
+              let sameDetails = [currItem.email, currItem.value].includes(itemExists?.email || itemExists?.username || '');
+              let confirmMessage = 'add to passvault? ' + (currItem.email || currItem.username);
+              if (!sameDetails) {
+                confirmMessage = 'update password for: ' + currItem.origin;
+              }
+              // check if either item exists and same details found or no existing item
+              if (((itemExists && !sameDetails) || !itemExists) && window.confirm(confirmMessage)) {
+                chrome.storage.local.get("saved").then(function (localData) {
+                  let savedLocal = [];
+                  localData.saved && localData.saved.forEach((elem: object) => savedLocal.push(elem));
+                  let webUrl = new URL(String(currTabInfo?.url)).origin;
+                  savedLocal.push({ email: currItem.email, username: currItem.username, value: currItem.value, web_url: webUrl, favIconUrl: currTabInfo?.favIconUrl, itemExists: !!itemExists })
+                  chrome.storage.local.set({ saved: savedLocal })
+                  sessionStorage.removeItem('currItem');
+                  clearInterval(listenerInterval);
+                })
+              } else clearInterval(listenerInterval);
+            }
+          } else {
+            // test listener
+            let __currItem = { origin: window.location.origin, pathname: window.location.pathname, value: '', email: '', username: '' };
+            sessionStorage.setItem('currItem', JSON.stringify(__currItem));
+            let inputs = document.querySelectorAll("input");
+            inputs.forEach(function (input) {
+              input.addEventListener('change', function (event) {
+                console.log('listener working');
+                let currItem: CurrItem = JSON.parse((sessionStorage.getItem('currItem') || '{}'));
+                if (input.type === 'password') currItem.value = input.value;
+                if (input.type === 'email') currItem.email = input.value;
+                if (input.type === 'text') {
+                  // check if email and save
+                  if (String(input.value).toLowerCase().match(/\S+@\S+\.\S+/)) currItem.email = input.value;
+                  // test check username
+                  if (!currItem.email && ['user', 'name', 'id'].some(x => input.getAttribute('name')?.includes(x))) currItem.username = input.value;
+                }
+                sessionStorage.setItem('currItem', JSON.stringify(currItem));
+              })
+            })
+          }
+        } catch (error) {
+          clearInterval(listenerInterval);
+          console.log('error occurred');
+        }
+      }, 2000)
       // select all form elements on the page and add onsubmit listeners
-      let formsArray = document.querySelectorAll("form");
-      formsArray.forEach(formElem => {
-        formElem.addEventListener('submit', function (e) {
-          let inputs = formElem.getElementsByTagName("input")
-          for (let index = 0; index < inputs.length; index++) {
-            const element = inputs[index];
-            let inputType = element.getAttribute('type');
-            // find password fields first
-            if (inputType === 'password') { response.value = element.value; }
-            // find email fields next
-            if (['text', 'email'].includes(String(inputType))) {
-              const validateEmail = (email: string) => {
-                // eslint-disable-next-line no-useless-escape
-                return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-              };
-              if (validateEmail(element.value)) { response.email = element.value; }
-            }
-          }
-          //  if no email fields found then find other fields - e.g username
-          if (response.email === '') {
-            let inputsArr = Array.from(inputs).filter(item => (item.getAttribute('type') !== 'password'));
-            if (inputsArr.length > 0) {
-              inputsArr.forEach(item => {
-                if (item.getAttribute('type') === 'text') { response.username = item.value }
-              })
-            }
-          }
-
-          let currTabDomain = new URL(String(currTabInfo.url));
-          let itemExists = vaultItems.find(x => x.web_url === currTabDomain.origin);
-          if (response.value) {
-            let confirmMessage = 'add to passvault? ' + response.email;
-            let sameDetails = [response.email, response.username].includes(itemExists?.email || itemExists?.username || '');
-            if (!sameDetails) {
-              confirmMessage = `update password for: ${currTabDomain.origin}`;
-            }
-
-            // check if either item exists and same details found or no existing item
-            if (((itemExists && !sameDetails) || !itemExists) && window.confirm(confirmMessage)) {
-              chrome.storage.local.get("saved").then(function (localData) {
-                let savedLocal = [];
-                localData.saved && localData.saved.forEach((elem: object) => savedLocal.push(elem));
-                let webUrl = new URL(String(currTabInfo?.url)).origin;
-                savedLocal.push({ ...response, web_url: webUrl, favIconUrl: currTabInfo?.favIconUrl, itemExists: !!itemExists })
-                chrome.storage.local.set({ saved: savedLocal })
-              })
-            }
-          }
-        })
-      })
     }
     // TODO: fix background autofill password for users
     if (message.type === 'AUTOFILL_DOM') {
-      if (vaultItems) {
-        for (let index = 0; index < vaultItems.length; index++) {
-          const item = vaultItems[index];
-          let domain = new URL(String(currTabInfo.url));
-          if (domain.origin === item.web_url) {
-            let inputs = document.getElementsByTagName('input');
-            let foundPasswordFields = [].filter.call(inputs, (el: HTMLElement) => el.getAttribute('type') === 'password');
-            if (foundPasswordFields.length > 0) {
-              // fix autofill
-              if (window.confirm('do you want to autofill your saved password?')) {
-                for (let index = 0; index < inputs.length; index++) {
-                  const element = inputs[index];
-                  let inputType = element.getAttribute('type');
-                  // find password fields first
-                  if (inputType === 'password') { element.value = item.value; }
-                  // find email field next
-                  if (inputType === 'email') { element.value = item.email }
-                  // else find username
-                  if (inputType === 'text') {
-                    element.value = item.email === '' ? item.username : item.email;
+      var autofillInterval = setInterval(function () {
+        try {
+          if (vaultItems) {
+            for (let index = 0; index < vaultItems.length; index++) {
+              const item = vaultItems[index];
+              let domain = new URL(String(currTabInfo.url));
+              if (domain.origin === item.web_url) {
+                let inputs = document.getElementsByTagName('input');
+                let foundPasswordFields = [].filter.call(inputs, (el: HTMLElement) => el.getAttribute('type') === 'password');
+                if (foundPasswordFields.length > 0) {
+                  // fix autofill
+                  if (window.confirm('do you want to autofill your saved password?')) {
+                    for (let index = 0; index < inputs.length; index++) {
+                      const element = inputs[index];
+                      let inputType = element.getAttribute('type');
+                      // find password fields first
+                      if (inputType === 'password') { element.value = item.value; }
+                      // find email field next
+                      if (inputType === 'email') { element.value = item.email }
+                      // else find username
+                      if (inputType === 'text') {
+                        element.value = item.email === '' ? item.username : item.email;
+                      }
+                    }
+                    clearInterval(autofillInterval);
+                  } else {
+                    clearInterval(autofillInterval);
                   }
                 }
+                break;
               }
             }
-            break;
           }
+        } catch (error: any) {
+          clearInterval(autofillInterval);
+          console.log('error occurred: ' + error.message);
+
         }
-      }
+      }, 2000)
     }
   })
   // })
